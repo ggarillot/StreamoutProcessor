d6bdbc461640a734301a8553d806e3aafc165002
Antoine Pingault <antoine.pingault@gmail.com>
2017-03-20 09:41:49 +0100

    Remove autoXML generation -> No flexibility

--- steer/streamout.py
+++ steer/streamout.py
@@ -10,83 +10,79 @@ import sys
 import time
 import subprocess
 import shlex # split properly command line for Popen
-from lxml import etree
+# from lxml import etree
 
 # Import default config file
 # Not needed here just for dumb editor not to complain about config not existing
 import config_streamout as config
+# # -----------------------------------------------------------------------------
+# def xmlValueBuilder(rootHandle, xmlHandleName, value, parameterType=None, option=None, optionValue=None, xmlParList=None):
+#     ''' Generate Marlin compliant xml options
+#     '''
+#     xmlHandle = etree.SubElement(rootHandle, "parameter", name=xmlHandleName)
+#     if parameterType is not None:
+#         xmlHandle.set("type", parameterType)
+#     if option is not None:
+#         xmlHandle.set(option, optionValue)
+#     xmlHandle.text = value
+#     xmlParList[xmlHandleName] = value
 
 
-'''
-'''
-# -----------------------------------------------------------------------------
-def xmlValueBuilder(rootHandle, xmlHandleName, value, parameterType=None, option=None, optionValue=None, xmlParList=None):
-    xmlHandle = etree.SubElement(rootHandle, "parameter", name=xmlHandleName)
-    if parameterType is not None:
-        xmlHandle.set("type", parameterType)
-    if option is not None:
-        xmlHandle.set(option, optionValue)
-    xmlHandle.text = value
-    xmlParList[xmlHandleName] = [value]
+# # -----------------------------------------------------------------------------
+# def generateXML(inputFiles, outputFile, parList, xmlFile):
+# ''' Generate xml file for Marlin ... Too specific for proper reusability even with yaml conf file
+# '''
+#     # generate XML
+#     ## TAG: Execute
+#     marlin = etree.Element('marlin')
+#     execute = etree.SubElement(marlin, "execute")
+#     for proc in conf.processorList:
+#         processor = etree.SubElement(execute, "processor", name=proc[0])
 
+#     ## TAG: Global
+#     glob = etree.SubElement(marlin, "global")
 
-'''
-'''
-# -----------------------------------------------------------------------------
-def generateXML(inputFiles, outputFile, parList):
-    # generate XML
-    ## TAG: Execute
-    marlin = etree.Element('marlin')
-    execute = etree.SubElement(marlin, "execute")
-    for proc in config.processorList:
-        processor = etree.SubElement(execute, "processor", name=proc[0])
+#     # -- Processor Parameters
+#     xmlValueBuilder(glob, "LCIOInputFiles", inputFiles, xmlParList=parList)
+#     # --- Max number of evts to process
+#     xmlValueBuilder(glob, "MaxRecordNumber", str(conf.maxEvt), xmlParList=parList)
+#     # --- Number of evts to skip
+#     xmlValueBuilder(glob, "SkipNEvents", str(conf.nSkipEvt), xmlParList=parList)
+#     # --- Verbosity
+#     xmlValueBuilder(glob, "Verbosity", conf.verbosity, option="options", optionValue="DEBUG0-4,MESSAGE0-4,WARNING0-4,ERROR0-4,SILENT", xmlParList=parList)
 
-    ## TAG: Global
-    glob = etree.SubElement(marlin, "global")
+#     ## TAG: Processors
+#     for proc in conf.processorList:
+#         processor = etree.SubElement(marlin, "processor", name=proc[0])
+#         processor.set("type", proc[1])
 
-    # -- Processor Parameters
-    xmlValueBuilder(glob, "LCIOInputFiles", inputFiles, xmlParList=parList)
-    # --- Max number of evts to process
-    xmlValueBuilder(glob, "MaxRecordNumber", str(config.maxEvt), xmlParList=parList)
-    # --- Number of evts to skip
-    xmlValueBuilder(glob, "SkipNEvents", str(config.nSkipEvt), xmlParList=parList)
-    # --- Verbosity
-    xmlValueBuilder(glob, "Verbosity", config.verbosity, option="options", optionValue="DEBUG0-4,MESSAGE0-4,WARNING0-4,ERROR0-4,SILENT", xmlParList=parList)
+#         # --- Verbosity
+#         xmlValueBuilder(processor, "Verbosity", conf.verbosity, parameterType="string", option="options", optionValue="DEBUG0-4,MESSAGE0-4,WARNING0-4,ERROR0-4,SILENT", xmlParList=parList)
+#         xmlValueBuilder(processor, "InputCollectionName", conf.inputCollectionName, "string", "lcioInType", conf.inputCollectionType, xmlParList=parList)
+#         xmlValueBuilder(processor, "OutputCollectionName", conf.outputCollectionName, "string", "lcioOutType", conf.inputCollectionType, xmlParList=parList)
+#         xmlValueBuilder(processor, "LCIOOutputFile", outputFile + ".slcio", "string", xmlParList=parList)
+#         xmlValueBuilder(processor, "ROOTOutputFile", outputFile + ".root", "string", xmlParList=parList)
+#         xmlValueBuilder(processor, "PlotFolder", conf.plotPath, "string", xmlParList=parList)
+#         # --- Byte shift for raw data reading
+#         xmlValueBuilder(processor, "RU_SHIFT", str(conf.ruShift), "int", xmlParList=parList)
+#         # --- XDAQ Byte shift for raw data reading
+#         xmlValueBuilder(processor, "XDAQ_SHIFT", str(conf.xDaqShift), "int", xmlParList=parList)
+#         xmlValueBuilder(processor, "CerenkovDifId", str(conf.cerenkovDifId), "int", xmlParList=parList)
+#         # --- Drop first Trigger event (bool)
+#         xmlValueBuilder(processor, "DropFirstRU", str(conf.dropRu), "bool", xmlParList=parList)
+#         # --- Skip full Asic event (bool)
+#         xmlValueBuilder(processor, "SkipFullAsic", str(conf.skipFullAsic), "bool", xmlParList=parList)
+#         xmlValueBuilder(processor, "Before2016Data", str(conf.before2016Data), "bool", xmlParList=parList)
+#         if conf.before2016Data is False:
+#             xmlValueBuilder(processor, "TreatEcal", str(conf.treatEcal), "bool", xmlParList=parList)
+#             # --- DetectorId for Ecal Data
+#             xmlValueBuilder(processor, "EcalDetectorIds", str(conf.EcalDetectorIds), "std::vector<int>", xmlParList=parList)
 
-    ## TAG: Processors
-    for proc in config.processorList:
-        processor = etree.SubElement(marlin, "processor", name=proc[0])
-        processor.set("type", proc[1])
+#     # pretty string
+#     s = etree.tostring(marlin, pretty_print=True)
+#     with open(xmlFile, "w") as outFile:
+#         outFile.write(s)
 
-        # --- Verbosity
-        xmlValueBuilder(processor, "Verbosity", config.verbosity, parameterType="string", option="options", optionValue="DEBUG0-4,MESSAGE0-4,WARNING0-4,ERROR0-4,SILENT", xmlParList=parList)
-        xmlValueBuilder(processor, "InputCollectionName", config.inputCollectionName, "string", "lcioInType", config.inputCollectionType, xmlParList=parList)
-        xmlValueBuilder(processor, "OutputCollectionName", config.outputCollectionName, "string", "lcioOutType", config.inputCollectionType, xmlParList=parList)
-        xmlValueBuilder(processor, "LCIOOutputFile", outputFile + ".slcio", "string", xmlParList=parList)
-        xmlValueBuilder(processor, "ROOTOutputFile", outputFile + ".root", "string", xmlParList=parList)
-        xmlValueBuilder(processor, "PlotFolder", config.plotPath, "string", xmlParList=parList)
-        # --- Byte shift for raw data reading
-        xmlValueBuilder(processor, "RU_SHIFT", str(config.ruShift), "int", xmlParList=parList)
-        # --- XDAQ Byte shift for raw data reading
-        xmlValueBuilder(processor, "XDAQ_SHIFT", str(config.xDaqShift), "int", xmlParList=parList)
-        xmlValueBuilder(processor, "CerenkovDifId", str(config.cerenkovDifId), "int", xmlParList=parList)
-        # --- Drop first Trigger event (bool)
-        xmlValueBuilder(processor, "DropFirstRU", str(config.dropRu), "bool", xmlParList=parList)
-        # --- Skip full Asic event (bool)
-        xmlValueBuilder(processor, "SkipFullAsic", str(config.skipFullAsic), "bool", xmlParList=parList)
-        xmlValueBuilder(processor, "Before2016Data", str(config.before2016Data), "bool", xmlParList=parList)
-        if config.before2016Data is False:
-            xmlValueBuilder(processor, "TreatEcal", str(config.treatEcal), "bool", xmlParList=parList)
-            # --- DetectorId for Ecal Data
-            xmlValueBuilder(processor, "EcalDetectorIds", str(config.EcalDetectorIds), "std::vector<int>", xmlParList=parList)
-
-    # pretty string
-    s = etree.tostring(marlin, pretty_print=True)
-    with open(config.xmlFile, "w") as outFile:
-        outFile.write(s)
-
-'''
-'''
 # -----------------------------------------------------------------------------
 def findNumberOfFiles(folder, findString):
     nFile = 0
@@ -228,14 +224,7 @@ def main():
             print ("[Streamout.py] - OutputFile already present...exiting")
             return
                     
-        xmlParameterList = {}
-        generateXML(inputFiles, outputFile, xmlParameterList)
 
-        print("\n[Streamout.py] ========================")
-        print("[Streamout.py] --- Dumping xml parameters: ")
-        for par in xmlParameterList:
-            print ("[Streamout.py] \t\t%s:\t%s" % (par, xmlParameterList[par]))
-        print("[Streamout.py] ========================\n")
 
         log = open(config.logFile % (config.logPath, runNumber), "w", 1) # line-buffered
         print("\n[Streamout.py] ========================")
